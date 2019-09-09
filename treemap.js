51424d8f14bc593864eae473069cefa441c73042
James Goodspeed <james.goodspeed@libertymutual.com>
2019-09-09 13:48:15 -0400

    code clean-up

--- treemap.js
+++ treemap.js
@@ -4,15 +4,13 @@ function makeTreeMap() {
   let width  = 900
   let mapDepth = 3
   let rootNode
-  let parent
-  let grandparent
-  let oldies = ['Liberty Mutual']
+  let ancestors = ['Liberty Mutual']
 
   let treemapLayout = d3.treemap()
     .size([width, height])
-    .paddingOuter(16);
+    .paddingOuter(16)
 
-    d3.json("health.json").then(function(data) {
+    d3.json('health.json').then(function(data) {
 
       // Do a deep search on the data object by node name
       let deepSearch = (object, name) => {
@@ -35,18 +33,19 @@ function makeTreeMap() {
 
 
 
-      let update = (d, ancestors) => {
+      let update = (d, name) => {
 
         // if name gets passed the user clicked on the top level node
-        if (ancestors && data.children !== null) {
+        if (name && data.children !== null) {
           let oldNodes = d3.selectAll('g')
 
           // remove all nodes and start over
           oldNodes
             .remove()
 
-          rootNode = d3.hierarchy(deepSearch(data, oldies[oldies.length - 2]))
-          oldies.pop()
+          // the new root node is the 2nd to last in the ancestors array
+          rootNode = d3.hierarchy(deepSearch(data, ancestors[ancestors.length - 2]))
+          ancestors.pop()
         } else {
           rootNode = d3.hierarchy(d)
         }
@@ -54,23 +53,18 @@ function makeTreeMap() {
 
         // sum and sort the root node values
         rootNode
-          .sum(function(d) {
-            return d.value;
-          })
-          .sort(function(a, b) {
-            return b.height - a.height || b.value - a.value;
-          });
+          .sum(d => d.value)
+          .sort((a,b) => b.height - a.height || b.value - a.value)
 
-        treemapLayout(rootNode);
+        treemapLayout(rootNode)
 
         // bind the data to the nodes
         let nodes = d3.select('svg')
           .attr('width', width)
           .attr('height', height)
           .selectAll('g')
-          .data(rootNode.descendants(), function(d) {
-            return d.data.name;
-          })
+          .data(rootNode.descendants(), d => d.data.name)
+
 
         nodes
           .exit()
@@ -78,35 +72,25 @@ function makeTreeMap() {
 
         let newNodes = nodes
           .enter()
-          .filter(function(d) {
-            return d.depth < mapDepth;
-          })
+          .filter(d => d.depth < mapDepth)
           .append('g')
-          .attr('transform', function(d) {
-            return 'translate(' + [d.x0, d.y0] + ')'
-          })
+          .attr('transform', d => 'translate(' + [d.x0, d.y0] + ')')
 
 
         newNodes
           .append('rect')
           .transition()
           .duration(750)
-          .attr('width', function(d) {
-            return d.x1 - d.x0;
-          })
-          .attr('height', function(d) {
-            return d.y1 - d.y0;
-          })
-          .attr('style', function(d) {
-            return ('fill:' + d3.interpolateRdYlGn(d.data.health))
-          })
+          .attr('width', d => d.x1 - d.x0)
+          .attr('height', d => d.y1 - d.y0)
+          .attr('style', d => ('fill:' + d3.interpolateRdYlGn(d.data.health)))
 
         newNodes
           // drill in on click
           .on('click', function(d) {
             if(d.depth === 0) { update(data, 'dummy') }
 
-            oldies.indexOf(d.parent.data.name) === -1 ? oldies.push(d.parent.data.name) : console.log("This item already exists");
+            ancestors.indexOf(d.parent.data.name) === -1 ? ancestors.push(d.parent.data.name) : console.log("This item already exists");
             update(d.parent.data)
 
             })
@@ -121,36 +105,23 @@ function makeTreeMap() {
           .transition()
           .duration(750)
           .attr('opacity',1)
-
-          .text(function(d) {
-            return d.data.name;
-          })
+          .text(d => d.data.name)
 
         nodes
           .merge(newNodes)
-          .filter(function(d) {
-            return d.depth < mapDepth;
-          })
+          // how many layers to show at once
+          .filter(d => d.depth < mapDepth)
           .transition()
           .duration(750)
-          .attr('transform', function(d) {
-            return 'translate(' + [d.x0, d.y0] + ')'
-          })
+          .attr('transform', d => 'translate(' + [d.x0, d.y0] + ')')
           .select('rect')
-          .attr('width', function(d) {
-            return d.x1 - d.x0;
-          })
-          .attr('height', function(d) {
-            return d.y1 - d.y0;
-          })
-          .attr('style', function(d) {
-            return ('fill:' + d3.interpolateRdYlGn(d.data.health))
-          })
+          .attr('width', d=> d.x1 - d.x0)
+          .attr('height', d => d.y1 - d.y0)
+          .attr('style', d => ('fill:' + d3.interpolateRdYlGn(d.data.health)))
 
       }
 
-    update(data);
+    update(data)
 
-  });
-
+  })
 }
